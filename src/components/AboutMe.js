cce48132488ec21a3354a393649528dd4d2b61ae
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2019-08-20 17:33:15 -0400

    Beginning to fill in placeholder text

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -92,7 +92,7 @@ export default class AboutMe extends Component {
                                 <div className="card-header" role="tab" id="headingOne">
                                     <h4 className="mb-0">
                                     <a data-toggle="collapse" data-parent="#accordion" href="#collapseOne" aria-expanded="true" aria-controls="collapseOne" className="" onClick={this.handleClick}>
-                                        <i className="title-icon fa fa-university"></i>Education<i className="collapse-icon fas fa-angle-up"></i>
+                                        <i className="title-icon fa fa-university"></i>Education<i className="collapse-icon fas fa-angle-down"></i>
                                     </a>
                                     </h4>
                                 </div>
@@ -112,7 +112,8 @@ export default class AboutMe extends Component {
                                 </div>
                                 <div id="collapseTwo" className="collapse accordianLinkHook" role="tabpanel" aria-labelledby="headingTwo">
                                     <div className="card-block light-gray-bg">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue dui vehicula, tincidunt diam ac, faucibus arcu. Etiam pellentesque imperdiet elit, id bibendum lorem auctor a. Donec rhoncus euismod pellentesque. Sed pretium semper nisi. Proin euismod euismod justo, quis mattis nulla iaculis nec. Integer at dolor ante. Donec efficitur suscipit nunc lobortis molestie.
+                                    <h4>Georgia Tech Research Institute</h4>
+                                    Professionally, I am a software research engineer and the head of the Sensor Visualization branch at the Georgia Tech Research Institute.  I primarily work as an embedded C++ developer with experience in various Real Time Operating Systems (Specifically, GreenHills INTEGRITY-178 and LynxOS-178).  The bulk of my career has been in support of the Department of Defense's (DoD) effort to reduce the acquisition cost of software capabilities for future avionics platforms.  I am a credited writer for the Future Airborne Capabilities Environment (FACE) Technical Standard, which aims to alleviate future software costs by standardizing the method in which software is written in order to promote competition and reuse of previously developed capabilities.
                                     </div>
                                 </div>
                             </div>
@@ -126,7 +127,9 @@ export default class AboutMe extends Component {
                                 </div>
                                 <div id="collapseThree" className="collapse accordianLinkHook" role="tabpanel" aria-labelledby="headingThree">
                                     <div className="card-block light-gray-bg">
-                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi congue dui vehicula, tincidunt diam ac, faucibus arcu. Etiam pellentesque imperdiet elit, id bibendum lorem auctor a. Donec rhoncus euismod pellentesque. Sed pretium semper nisi. Proin euismod euismod justo, quis mattis nulla iaculis nec. Integer at dolor ante. Donec efficitur suscipit nunc lobortis molestie.
+                                    For me, web development has been and continues to be a long-standing side hustle.  I am excited about creating products which people will actively use and even more so I am passionate about enabling individuals and small business to receive the benefits which modern technology has to offer!
+
+                                    A detailed portfolio of my web development projects can be seen below in the portfolio section.
                                     </div>
                                 </div>
                             </div>
